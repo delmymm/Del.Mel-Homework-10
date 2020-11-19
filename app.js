26b14adba12307046db8ca844e2f40cd4bcbd5e1
Delmy Melendez <delmymm93@gmail.com>
2020-11-19 12:15:25 -0700

    Code added to app.js

--- app.js
+++ app.js
@@ -20,15 +20,38 @@ inquirer
       name: 'name',
     },
     {
-      type: 'input',
+      type: 'list',
       message: 'What is your role',
       name: 'role',
+      choices: [
+        "Engineer",
+        "Intern",
+        "Manager"
+      ],
     },
     {
       type: 'input',
       message: 'What is your ID number?',
       name: 'id',
     },
+    {
+      type: 'input',
+      message: 'What is your email?',
+      name: 'email',
+    }
   ]).then(function (response) {
-
-}); 
+    let generateHTML = `# ${response.title}\n
+    ${response.name}\n
+    ${response.role}\n
+    ${response.id}\n
+    ${response.email}\n
+    `
+    writeToFile('team.html', generateHTML)
+  })
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return;
+    };
+  });
+}; 
